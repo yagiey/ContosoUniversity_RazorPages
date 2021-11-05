045564c8688ae4c2e8bced357643cd8a4b30e17b
yagiey <yagiey@gmail.com>
2021-11-05 14:54:00 +0900

    after finished: https://docs.microsoft.com/en-us/aspnet/core/data/ef-rp/intro?view=aspnetcore-5.0&tabs=visual-studio#scaffold-student-pages

---
+++ ContosoUniversity_RazorPages/Data/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity_RazorPages.Models;
+
+namespace ContosoUniversity_RazorPages.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContosoUniversity_RazorPages.Models.Student> Student { get; set; }
+    }
+}
---
+++ ContosoUniversity_RazorPages/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity_RazorPages.Data;
+using ContosoUniversity_RazorPages.Models;
+
+namespace ContosoUniversity_RazorPages.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity_RazorPages.Data.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity_RazorPages.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Student.ToListAsync();
+        }
+    }
+}
